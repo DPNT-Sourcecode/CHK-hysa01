14aeb35a49037c2ecabf57dc6b390938c90c33cd
sbx_user1051 <sbx_user1051@169.254.11.93>
2022-05-01 19:47:06 +0000

    Sun May 01 19:47:06 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,24 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+    //        Our price table and offers:
+    //+------+-------+----------------+
+    //| Item | Price | Special offers |
+    //+------+-------+----------------+
+    //| A    | 50    | 3A for 130     |
+    //| B    | 30    | 2B for 45      |
+    //| C    | 20    |                |
+    //| D    | 15    |                |
+    //+------+-------+----------------+
+        Map<String, Integer> itemList = new HashMap<>();
+        itemList.put("A", 50);
+        itemList.put("B", 30);
+        itemList.put("C", )
     }
 }
+
