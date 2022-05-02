86af95ada48fcf8371687fedae71b9180264a49f
sbx_user1051 <sbx_user1051@169.254.224.165>
2022-05-02 14:50:15 +0000

    Mon May 02 14:50:15 UTC 2022

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -20,10 +20,42 @@ public class CheckoutSolutionTest {
     public void testIllegalInputLowerCase() {
         assertEquals(-1, checkoutSolution.checkout("abab").intValue());
         assertEquals(-1, checkoutSolution.checkout("gfhj").intValue());
+    }
 
+    @Test
+    public void testIllegalInputNonExistentItem() {
+        assertEquals(-1, checkoutSolution.checkout("EFGHIJ").intValue());
+        assertEquals(-1, checkoutSolution.checkout("ABEFG").intValue());
+    }
+
+    @Test
+    public void testIllegalInputZeroItems() {
+        assertEquals(-1, checkoutSolution.checkout("").intValue());
+    }
+
+    @Test
+    public void testNoOfferItems() {
+        assertEquals(55, checkoutSolution.checkout("CDC").intValue());
+        assertEquals(15, checkoutSolution.checkout("D").intValue());
+    }
+
+    @Test
+    public void testOneOfferItem() {
+        assertEquals(85, checkoutSolution.checkout("ACD").intValue());
+        assertEquals(150, checkoutSolution.checkout("ACAA").intValue());
+        assertEquals(200, checkoutSolution.checkout("ACAAA").intValue());
+    }
+
+    @Test
+    public void testTwoOfferItems() {
+        assertEquals(80, checkoutSolution.checkout("AB").intValue());
+        assertEquals(145, checkoutSolution.checkout("ABAB").intValue());
+        assertEquals(175, checkoutSolution.checkout("AAABB").intValue());
+        assertEquals(290, checkoutSolution.checkout("ABBBACDAA").intValue());
 
     }
 
 }
+
 
 
