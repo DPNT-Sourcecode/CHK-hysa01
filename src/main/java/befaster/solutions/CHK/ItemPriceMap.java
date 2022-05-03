247fbde9aeb34b016c38470ab3c2914f02a0869c
sbx_user1051 <sbx_user1051@169.254.173.89>
2022-05-03 03:55:55 +0000

    Tue May 03 03:55:55 UTC 2022

--- src/main/java/befaster/solutions/CHK/ItemPriceMap.java
+++ src/main/java/befaster/solutions/CHK/ItemPriceMap.java
@@ -12,6 +12,28 @@ public class ItemPriceMap {
         itemPriceMap.put('D', 15);
         itemPriceMap.put('E', 40);
         itemPriceMap.put('F', 10);
+        itemPriceMap.put('G', 20);
+        itemPriceMap.put('H', 10);
+        itemPriceMap.put('I', 35);
+        itemPriceMap.put('J', 60);
+        itemPriceMap.put('K', 80);
+        itemPriceMap.put('L', 90);
+        itemPriceMap.put('M', 15);
+        itemPriceMap.put('N', 40);
+        itemPriceMap.put('O', 10);
+        itemPriceMap.put('P', 50);
+        itemPriceMap.put('Q', 30);
+        itemPriceMap.put('R', 50);
+        itemPriceMap.put('S', 30);
+        itemPriceMap.put('T', 20);
+        itemPriceMap.put('U', 40);
+        itemPriceMap.put('V', 50);
+        itemPriceMap.put('W', 20);
+        itemPriceMap.put('X', 90);
+        itemPriceMap.put('Y', 10);
+        itemPriceMap.put('Z', 50);
+
     }
 
 }
+
