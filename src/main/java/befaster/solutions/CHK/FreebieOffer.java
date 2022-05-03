72e6965a8b67052db204a6fa98a22e621250c0ae
sbx_user1051 <sbx_user1051@169.254.224.165>
2022-05-03 01:38:21 +0000

    Tue May 03 01:38:21 UTC 2022

--- src/main/java/befaster/solutions/CHK/FreebieOffer.java
+++ src/main/java/befaster/solutions/CHK/FreebieOffer.java
@@ -1,4 +1,25 @@
 package befaster.solutions.CHK;
 
 public class FreebieOffer {
+    private Character name, freebieItem;
+    private Integer quantity, freebieItemQuantity;
+
+    public FreebieOffer(Character name, Integer quantity, Character freebieItem, Integer freebieItemQuantity) {
+        this.name = name;
+        this.quantity = quantity;
+        this.freebieItem = freebieItem;
+        this.freebieItemQuantity = freebieItemQuantity;
+    }
+
+    public Character getFreebieItem() {
+        return freebieItem;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Integer getFreebieItemQuantity() {
+        return freebieItemQuantity;
+    }
 }
