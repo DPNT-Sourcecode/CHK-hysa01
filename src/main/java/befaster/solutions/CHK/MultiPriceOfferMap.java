d7ed4c8a48a8eab68c85616ab3b0730c47c62422
sbx_user1051 <sbx_user1051@169.254.224.165>
2022-05-03 01:44:21 +0000

    Tue May 03 01:44:21 UTC 2022

--- src/main/java/befaster/solutions/CHK/MultiPriceOfferMap.java
+++ src/main/java/befaster/solutions/CHK/MultiPriceOfferMap.java
@@ -1,14 +1,24 @@
 package befaster.solutions.CHK;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class MultiPriceOfferMap {
-    public static Map<Character, MultiPriceOffer> offerPriceMap = new HashMap<>();
+    public static Map<Character, ArrayList> multiPriceOfferMap = new HashMap<>();
     static {
-        offerPriceMap.put('A', new MultiPriceOffer('A', 3, 130));
-        offerPriceMap.put('B', new MultiPriceOffer('B', 2, 45));
+        ArrayList<MultiPriceOffer> multiPriceOfferlistA = new ArrayList<>();
+        ArrayList<MultiPriceOffer> multiPriceOfferlistB = new ArrayList<>();
+
+        multiPriceOfferlistA.add(new MultiPriceOffer('A', 5, 200));
+        multiPriceOfferlistA.add(new MultiPriceOffer('A', 3, 130));
+        multiPriceOfferlistA.add(new MultiPriceOffer('B', 2, 45));
+
+
+
+        multiPriceOfferMap.put('A', multiPriceOfferlistA);
+        multiPriceOfferMap.put('B', multiPriceOfferlistB);
     }
 
 }
